03b5dc3b057706f51624b3558c8a70b05dbed8f2
liangcheng <liangcheng@365yf.com>
2015-09-01 16:41:44 +0800

    添加工程

---
+++ Clock/Clock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Clock
+//
+//  Created by 梁成 on 15/9/1.
+//  Copyright (c) 2015年 梁成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Clock/Clock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Clock
+//
+//  Created by 梁成 on 15/9/1.
+//  Copyright (c) 2015年 梁成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
